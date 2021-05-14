a534c6d2ff515b5885b60e384e4666b7bc4d6c99
Lunarixus <Nathanb644@hotmail.co.uk>
2021-05-14 19:23:42 +0000

    sdm660-common: Remove unwanted GApps Packages Build
    Change-Id: Ie4f08459a6004bb16d7dfec7e7a152a42dc0ffdf
    Signed-off-by: dotkit <ewprjkt@proton.me>

--- sdm660.mk
+++ sdm660.mk
@@ -413,6 +413,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_SOONG_NAMESPACES += \
     vendor/qcom/opensource/usb/etc
 
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # Seccomp
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/seccomp/atfwd@2.0.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/atfwd@2.0.policy \
