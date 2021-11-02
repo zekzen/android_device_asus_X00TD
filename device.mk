113388923ff05507684a25ad95aa9f995fed4409
Albert I <kras@raphielgang.org>
2021-11-02 21:47:53 +0530

    X00TD: Inherit several Android Go configurations
    Beneficial for everyone, even on non-Go devices.
    
    Signed-off-by: Albert I <kras@raphielgang.org>
    Change-Id: I088fd00500ce10df5f413cb762aaed2ec00f5dfc
    Signed-off-by: Tiktodz <ewprjkt@proton.me>

--- device.mk
+++ device.mk
@@ -211,6 +211,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/permissions/privapp-permissions-hotword.xml:system/etc/permissions/privapp-permissions-hotword.xml
 
+# Inherit several Android Go Configurations(Beneficial for everyone, even on non-Go devices)
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+
 # Input
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl
