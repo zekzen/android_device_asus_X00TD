08fd6c7823bc9d175dea849f10775b67198fdeaa
LuK1337 <priv.luk@gmail.com>
2023-10-27 20:25:58 +0200

    sdm660-common: Build libprotobuf-cpp-lite-3.9.1-vendorcompat
    Change-Id: I0aac742db9c2b4b3e48ee20158f19405863893c2 | AOSP
    Signed-off-by: dotkit <ewprjkt@proton.me>

--- sdm660.mk
+++ sdm660.mk
@@ -472,7 +472,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libprotobuf-cpp-full-vendorcompat \
     libprotobuf-cpp-full-3.9.1-vendorcompat \
-    libprotobuf-cpp-lite-vendorcompat
+    libprotobuf-cpp-lite-vendorcompat \
+    libprotobuf-cpp-lite-3.9.1-vendorcompat
 
 # VNDK
 PRODUCT_EXTRA_VNDK_VERSIONS := 28 29 30
