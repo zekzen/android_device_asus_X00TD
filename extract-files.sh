a4f1f3ac49f7fee72b5cfeb1aea3136143a7ce44
LuK1337 <priv.luk@gmail.com>
2023-11-01 18:11:13 +0100

    X00TD: Use libutils-v33 for pm-service [1/2]
    Change-Id: Ia998fa72d8747de18010abe6491d2c374cb7aa0a
    Signed-off-by: dotkit <ewprjkt@proton.me>

--- extract-files.sh
+++ extract-files.sh
@@ -80,6 +80,10 @@ function blob_fixup() {
         "${PATCHELF}" --replace-needed "libprotobuf-cpp-lite-3.9.1.so" "libprotobuf-cpp-full-3.9.1.so" "${2}"
         ;;
 
+        vendor/bin/pm-service)
+        grep -q libutils-v33.so "${2}" || "${PATCHELF}" --add-needed "libutils-v33.so" "${2}"
+        ;;
+
     esac
 }
 
