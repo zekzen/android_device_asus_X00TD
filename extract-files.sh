51131975a4e7dc6f183885c1e3d7bc719d1c9124
L1ghtzin <lightzin021@gmail.com>
2023-12-09 16:07:50 -0300

    X00TD: Patch libraries to load protobuf
    Change-Id: I1e3809eea0b806aa8e33d8170ef7498df4b0d38a
    Signed-off-by: dotkit <ewprjkt@proton.me>

--- extract-files.sh
+++ extract-files.sh
@@ -75,6 +75,11 @@ function blob_fixup() {
         "${PATCHELF}" --remove-needed "libhidlbase.so" "${2}"
         ;;
 
+        # Use VNDK 29 protobuf
+        vendor/lib64/libwvhidl.so)
+        "${PATCHELF}" --replace-needed "libprotobuf-cpp-lite-3.9.1.so" "libprotobuf-cpp-full-3.9.1.so" "${2}"
+        ;;
+
     esac
 }
 
