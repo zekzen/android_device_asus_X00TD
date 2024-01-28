43af91ff5509d6a07fd66d23080cacadba5061ab
Anand S <anandzzz360@gmail.com>
2024-01-28 20:35:50 +0530

    sdm660-common: Switch back to binderized light hal
    Change-Id: I1f97856a777b5f523067da8421a4608839fdc67f
    Signed-off-by: dotkit <ewprjkt@proton.me>

--- sdm660.mk
+++ sdm660.mk
@@ -228,9 +228,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    lights.qcom \
-    android.hardware.light@2.0-impl \
-    android.hardware.light@2.0-service
+    android.hardware.light@2.0-service.asus_sdm660
 
 # LiveDisplay native
 PRODUCT_PACKAGES += \
