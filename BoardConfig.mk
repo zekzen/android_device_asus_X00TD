7647092b690cc08f1111ff71b98096236b1b5ea7
Bruno Martins <bgcngm@gmail.com>
2024-02-08 17:05:32 +0000

    X00TD: Use the common device compatibility matrix from QCOM
    Change-Id: I0c0f3ecddbe6d7b232cc0d13c78ca58094e4e720
    Signed-off-by: dotkit <ewprjkt@proton.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -87,7 +87,7 @@ LOC_HIDL_VERSION := 4.0
 # HIDL
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/framework_manifest.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
-DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
+DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 
 ODM_MANIFEST_SKUS += NFC
 ODM_MANIFEST_NFC_FILES := $(DEVICE_PATH)/manifest_nfc.xml
