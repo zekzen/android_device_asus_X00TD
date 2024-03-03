408a3ac87ea6294376652489d527597ba1cdcf79
dotkit <ewprjkt@proton.me>
2024-03-03 19:56:34 +0000

    sdm660-common: overlay: Add pinner files for SystemUI
    Change-Id: Idabdf4b92a4595f7c786760ee7b57b9fabe07776

--- sdm660.mk
+++ sdm660.mk
@@ -109,6 +109,10 @@ PRODUCT_COPY_FILES += \
 # DebugFS
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 
+# Dexpreopt
+PRODUCT_DEXPREOPT_SPEED_APPS += \
+    SystemUI
+
 # Display
 PRODUCT_PACKAGES += \
     gralloc.sdm660 \
