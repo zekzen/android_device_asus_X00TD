63614d706bc96d9472841f387410d626199b628a
Yumi Yukimura <me.cafebabe@gmail.com>
2024-03-21 23:55:45 +0800

    X00TD: Switch to USB 1.3-basic HAL
    Change-Id: I6f477db2220b3563b44bb9545b7889166b8a6b06
    Signed-off-by: Tiktodz <ewprjkt@proton.me>

--- device.mk
+++ device.mk
@@ -468,7 +468,7 @@ PRODUCT_COPY_FILES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.basic
+    android.hardware.usb@1.3-service.basic
 
 # Vibrator
 PRODUCT_PACKAGES += \
