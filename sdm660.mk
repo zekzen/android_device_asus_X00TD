c4983fdc39f9b9d8ef2104fa8b9ca35fe6470986
Tiktodz <ewprjkt@proton.me>
2024-04-14 06:45:31 +0000

    sdm660-common: Drop trust HAL
    Signed-off-by: Tiktodz <ewprjkt@proton.me>

--- sdm660.mk
+++ sdm660.mk
@@ -454,10 +454,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
-# Trust HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
-
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service.basic
