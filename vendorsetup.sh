6704d5e50e0ab55e7310c2a1abea14e335838209
Tiktodz <ewprjkt@proton.me>
2024-05-12 17:53:53 +0000

    X00TD: Add vendorsetup
    Change-Id: I954e016e5b90a49bcf33c75dc674eafce424900d

---
+++ vendorsetup.sh
@@ -0,0 +1,16 @@
+rm -rf kernel/asus/sdm660
+git clone --depth=1 --recursive https://github.com/Tiktodz/android_kernel_asus_sdm636 -b 14-ksu kernel/asus/sdm660
+rm -rf kernel/asus/sdm660/KernelSU/userspace
+
+rm -rf device/asus
+git clone --depth=1 https://github.com/sotodrom/device_asus_X00TD -b u device/asus/X00TD
+
+rm -rf vendor/asus
+git clone --depth=1 https://github.com/sotodrom/vendor_asus -b u vendor/asus
+
+cd frameworks/base
+git reset --merge 9778620
+git reset --merge HEAD~1
+cd ../../
+
+export TZ=Asia/Jakarta
