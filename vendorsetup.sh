720d1a882bc64cc634f9728aa1d03786fd46dc90
Tiktodz <ewprjkt@proton.me>
2024-05-23 04:03:26 +0000

    X00TD: Declare vendorsetup
    Change-Id: Iaa9d3bbe7ecf29efa05adfe0c861e5c49af80bdc

---
+++ vendorsetup.sh
@@ -0,0 +1,20 @@
+rm -rf device/asus
+git clone --depth=1 https://github.com/Tiktodz/android_device_asus_X00TD -b matrixx-4.19 device/asus/X00TD
+
+rm -rf vendor/asus
+git clone --depth=1 https://github.com/Tiktodz/android_vendor_asus_X00TD -b udc-4.19 vendor/asus
+
+rm -rf kernel/asus/sdm660
+git clone --depth=1 --recursive https://github.com/Tiktodz/android_kernel_asus_sdm660 -b sdm660 kernel/asus/sdm660
+rm -rf kernel/asus/sdm660/KernelSU/userspace
+
+rm -rf device/qcom/sepolicy-legacy-um
+git clone --depth=1 https://github.com/sotodrom/android_device_qcom_sepolicy -b lineage-21.0-legacy-um device/qcom/sepolicy-legacy-um
+
+rm -rf device/lineage/sepolicy
+git clone --depth=1 https://github.com/sotodrom/android_device_lineage_sepolicy -b 14 device/lineage/sepolicy
+
+rm -rf frameworks/base
+git clone --depth=1 https://github.com/Tiktodz/android_frameworks_base frameworks/base
+
+export TZ=Asia/Jakarta
