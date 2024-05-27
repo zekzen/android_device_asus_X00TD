33921aa4511d008abb807814d3ad9499512d0eed
dotkit <ewprjkt@proton.me>
2024-05-27 11:16:58 +0000

    X00TD: Update vendorsetup
    Change-Id: Ic1cf760a668ada550532d3e6579f98729929032b

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,7 +5,7 @@ rm -rf vendor/asus
 git clone --depth=1 https://github.com/Tiktodz/android_vendor_asus_X00TD -b udc-4.19 vendor/asus
 
 rm -rf kernel/asus/sdm660
-git clone --depth=1 --recursive https://github.com/Tiktodz/android_kernel_asus_sdm660 -b sdm660 kernel/asus/sdm660
+git clone --depth=1 --recursive https://github.com/Tiktodz/android_kernel_asus_sdm660 -b tree kernel/asus/sdm660
 rm -rf kernel/asus/sdm660/KernelSU/userspace
 
 rm -rf device/qcom/sepolicy-legacy-um
@@ -16,5 +16,7 @@ git clone --depth=1 https://github.com/sotodrom/android_device_lineage_sepolicy 
 
 rm -rf frameworks/base
 git clone --depth=1 https://github.com/Tiktodz/android_frameworks_base frameworks/base
+
+rm -rf vendor/lineage-priv
 
 export TZ=Asia/Jakarta
